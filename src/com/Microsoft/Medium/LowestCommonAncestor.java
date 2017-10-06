2ba6068969ab94d53010a957b99d346042af8859
Dhiraj Borade <dhirajborade@gmail.com>
2017-10-06 15:39:34 -0400

    Lowest Common Ancestor of Binary Tree

--- src/com/Microsoft/Medium/LowestCommonAncestor.java
+++ src/com/Microsoft/Medium/LowestCommonAncestor.java
@@ -2,7 +2,7 @@ package com.Microsoft.Medium;
 
 public class LowestCommonAncestor {
 
-	public class TreeNode {
+	private static class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
@@ -13,12 +13,29 @@ public class LowestCommonAncestor {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(5);
+		root.right = new TreeNode(1);
+		root.left.left = new TreeNode(6);
+		root.left.right = new TreeNode(2);
+		root.right.left = new TreeNode(0);
+		root.right.right = new TreeNode(8);
+		root.left.right.left = new TreeNode(7);
+		root.left.right.right = new TreeNode(4);
+		TreeNode p = root.left.left;
+		TreeNode q = root.left.right;
+		TreeNode resultNode = lowestCommonAncestor(root, p, q);
+		System.out.println(resultNode.val);
 	}
-	
-	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		return root;
-    }
+
+	private static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || root == p || root == q)
+			return root;
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (left != null && right != null)
+			return root;
+		return left != null ? left : right;
+	}
 
 }
--- src/com/Microsoft/Medium/SpiralMatrix.java
+++ src/com/Microsoft/Medium/SpiralMatrix.java
@@ -1,10 +1,42 @@
 package com.Microsoft.Medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpiralMatrix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+		int[][] matrix = {{3}, {2}};
+		List<Integer> resultList = spiralOrder(matrix);
+		System.out.println(resultList.toString());
+	}
 
+	private static List<Integer> spiralOrder(int[][] matrix) {
+		List<Integer> resultList = new ArrayList<Integer>();
+		if (matrix.length == 0) {
+			return resultList;
+		}
+		
+		int rowStart = 0;
+		int rowEnd = matrix.length - 1;
+		int colStart = 0;
+		int colEnd = matrix[0].length - 1;
+		
+		while (rowStart <= rowEnd && colStart <= colEnd) {
+			for (int i = colStart; i <= colEnd; i++) { resultList.add(matrix[rowStart][i]); }
+			for (int i = rowStart + 1; i <= rowEnd; i++) { resultList.add(matrix[i][colEnd]); }
+			if (rowStart < rowEnd && colStart < colEnd) {
+				for (int i = colEnd - 1; i > colStart; i--) { resultList.add(matrix[rowEnd][i]); }
+				for (int i = rowEnd; i > rowStart; i--) { resultList.add(matrix[i][colStart]); }
+			}
+			rowStart++;
+			rowEnd--;
+			colStart++;
+			colEnd--;
+			
+		}
+		return resultList;
 	}
 
 }
