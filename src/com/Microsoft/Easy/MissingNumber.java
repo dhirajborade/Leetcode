87203d664ed237412128e71e016049738b62ef88
Dhiraj Borade <dhirajborade@gmail.com>
2017-10-07 16:51:39 -0400

    Buy Sell Stock and Missing Number

---
+++ src/com/Microsoft/Easy/MissingNumber.java
@@ -0,0 +1,23 @@
+package com.Microsoft.Easy;
+
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		int[] numberArray = {0, 1, 2};
+		int result = missingNumber(numberArray);
+		System.out.println(result);
+	}
+	
+	private static int missingNumber(int[] nums) {
+		int xor = 0;
+		int i = 0;
+		
+		while (i < nums.length) {
+			xor = xor ^ i ^ nums[i];
+			i++;
+		}
+		
+		return xor ^ i;
+    }
+
+}
