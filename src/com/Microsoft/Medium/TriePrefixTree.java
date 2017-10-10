5329bb7cc7516a906c50f20267563aa36a8d288e
Dhiraj Borade <dhirajborade@gmail.com>
2017-10-10 18:18:02 -0400

    Trie Prefix Tree

---
+++ src/com/Microsoft/Medium/TriePrefixTree.java
@@ -0,0 +1,36 @@
+package com.Microsoft.Medium;
+
+public class TriePrefixTree {
+
+	/** Initialize your data structure here. */
+	public TriePrefixTree() {
+
+	}
+
+	/** Inserts a word into the trie. */
+	private static void insert(String word) {
+
+	}
+
+	/** Returns if the word is in the trie. */
+	private static boolean search(String word) {
+		return false;
+
+	}
+
+	/**
+	 * Returns if there is any word in the trie that starts with the given prefix.
+	 */
+	private static boolean startsWith(String prefix) {
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+		String word = "dhiraj";
+		String prefix = "d";
+		insert(word);
+		System.out.println(search(word));
+		System.out.println(startsWith(prefix));
+	}
+}
